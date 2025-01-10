235a02fda6505de222334c49e36b4e2c49f2decf
Archasmiel <95091020+Archasmiel@users.noreply.github.com>
2025-01-10 21:56:21 +0200

    unity 2d
    lesson 18

---
+++ unity-2d/project-3/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 startPos;
+    public Transform target;
+    private Vector3 targetPos;
+    public float speed;
+
+    private void Start() {
+        startPos = transform.position;
+    }
+
+    public void Update() {
+        if (target != null) {
+            targetPos = new Vector3(target.position.x, target.position.y,
+                transform.position.z);
+            Vector3 velocity = (targetPos - transform.position) * speed;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, 
+                ref velocity, 1.0f, Time.deltaTime);
+        }
+    }
+}
