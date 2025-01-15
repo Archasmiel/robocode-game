4474bff1a64c63371ee1adfee58b1645ecf01aec
Archasmiel <95091020+Archasmiel@users.noreply.github.com>
2025-01-15 17:45:27 +0200

    unity 2d
    lesson 4 fix
    lesson 5
    materials fix

--- unity-2d/project-1/Assets/Scripts/PlayerController.cs
+++ unity-2d/project-1/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
     [SerializeField] private Rigidbody2D rb2d;
     private bool isGrounded;
+    public int score;
+    public int health;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text hpText;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +31,25 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) {
         isGrounded = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Jewel")) {
+            score++;
+            Destroy(collision.gameObject);
+        }
+        if (collision.CompareTag("Obstacle")) {
+            health--;
+            collision.GetComponent<SpriteRenderer>().color = Color.red;
+            // collision.enabled = false;
+            if (health <= 0) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    private void LateUpdate() {
+        scoreText.text = score.ToString();
+        hpText.text = health.ToString();
     }
 }
